8d8fe93a35d9fc32e976cb1257985b0e3ba20561
Samith Hasan <hasan.samith@email.com>
2024-03-15 23:54:12 -0400

    Updated layout for code

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -14,14 +14,10 @@ def move_files(source_dir, target_dir, extension):
             shutil.move(os.path.join(source_dir, file), target_dir)
 
 
-# Move .txt files to 'TXT FILES' directory
 move_files(desktop_path, os.path.join(desktop_path, "TXT FILES"), ".txt")
 
-# Move .pdf files to 'PDF FILES' directory
 move_files(desktop_path, os.path.join(desktop_path, "PDF FILES"), ".pdf")
 
-# Move .jpg files to 'JPG FILES' directory
 move_files(desktop_path, os.path.join(desktop_path, "JPG FILES"), ".jpg")
 
-# Move .zip files to 'ZIP FILES' directory
 move_files(desktop_path, os.path.join(desktop_path, "ZIP FILES"), ".zip")
